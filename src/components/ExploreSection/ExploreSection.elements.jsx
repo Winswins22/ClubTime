538deb0c2d5a39f1acdfc3a2686296a87821e902
= <65200215+Ri-Hong@users.noreply.github.com>
2021-06-26 20:25:26 -0400

    Update ExploreSection.elements.jsx

---
+++ src/components/ExploreSection/ExploreSection.elements.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const HeaderSection = styled.div`
+  background-color: #03001c;
+
+`
+export const HeaderText = styled.h1`
+  background-color: #03001c;
+
+`
+
+export const HeaderSubText = styled.h2`
+  background-color: #03001c;
+
+`
